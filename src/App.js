5950e1f91a1fbcb1c378d6f38f014282448140a6
ezduzit4me <61637409+ezduzit4me@users.noreply.github.com>
2022-01-13 17:42:51 +1000

    aws version

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import { Amplify } from 'aws-amplify';
+import { useAuthenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
 
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+import { Home } from "./Home";
+import { Login } from "./Login";
+import "./styles.css";
 
-import awsExports from './aws-exports';
+import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
-function App({ signOut, user }) {
-  return (
-    <>
-      <h1>Hello {user.username}</h1>
-      <button onClick={signOut}>Sign out</button>
-    </>
-  );
-}
+export default function App() {
+  const { user } = useAuthenticator();
 
-export default withAuthenticator(App);
+  if (user) {
+    return <Home />;
+  }
+
+  return <Login />;
+}
