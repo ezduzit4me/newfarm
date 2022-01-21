048d8088d33bf532ecf3178a5098bb5592ef9278
ezduzit4me <61637409+ezduzit4me@users.noreply.github.com>
2022-01-21 21:09:22 +1000

    new auth setup

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,16 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "newfarmfa3d7944": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        },
+        "userPoolGroups": {
+            "newfarmGroupRole": "string"
+        }
+    }
+}
